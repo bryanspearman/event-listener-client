daa87c96db0eb38b5e70fee43699d8663ff55e44
Bryan Spearman <bryanspearman@yahoo.com>
2019-01-08 14:52:55 -0500

    itemDetails update

--- src/items/details/itemDetails.js
+++ src/items/details/itemDetails.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getItems } from '../item-actions';
-// import Counter from '../counter/counter';
+import Counter from '../counter/counter';
 
 export class ItemDetails extends Component {
   componentDidMount() {
@@ -11,6 +11,7 @@ export class ItemDetails extends Component {
   render() {
     console.log(this.props.index, this.props.item);
     const { index, item, item: { itemList = [] } = {} } = this.props;
+
     const title = this.props.item.itemList[this.props.index].itemTitle;
     const dateToUse = new Date(
       this.props.item.itemList[this.props.index].itemDate
@@ -20,8 +21,8 @@ export class ItemDetails extends Component {
     return item && index && itemList.length ? (
       <div id="item-details">
         <h1>{title}</h1>
-          <span className="item-date">{dateToUse}</span>
-             <Counter targetDate={dateToUse} />
+        <span className="item-date">{dateToUse}</span>
+        <Counter targetDate={dateToUse} />
         <div className="item-notes">
           <h2>Notes</h2>
           <p>{notes}</p>
