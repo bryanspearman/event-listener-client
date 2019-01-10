0df5db2e45261bbbac747afa2cdb797b00691935
Bryan Spearman <bryanspearman@yahoo.com>
2019-01-10 10:30:46 -0500

    stable itemListView with new syntax - no itemDetails onClick

--- src/items/dashboard/dashboard.js
+++ src/items/dashboard/dashboard.js
@@ -3,30 +3,15 @@ import requiresLogin from '../../auth/requires-login';
 import HeaderBar from '../../nav/header-bar';
 import SplashView from './splashView';
 import ItemListView from '../list/itemListView';
-import { getItems, selectItem } from '../item-actions';
-import { connect } from 'react-redux';
 
 export class Dashboard extends React.Component {
-  componentDidMount() {
-    this.props.getItems();
-    this.props.selectItem();
-    this.setSelectedItem = this.setSelectedItem.bind(this);
-  }
-  setSelectedItem = item => {
-    console.log(item);
-    return { selectedItem: item };
-  };
-
   render() {
     return (
       <div className="row">
         <div className="dashboard">
           <HeaderBar />
           <main role="main">
-            <ItemListView
-              itemList={this.props.itemList}
-              selectedItem={this.props.selectedItem}
-            />
+            <ItemListView />
             <div className="info-view">
               <SplashView />
             </div>
@@ -36,21 +21,5 @@ export class Dashboard extends React.Component {
     );
   }
 }
-const mapStateToProps = state => {
-  return {
-    itemList: state.item.itemList,
-    selectedItem: state.item.selectedItem
-  };
-};
 
-const mapDispatchToProps = {
-  getItems,
-  selectItem
-};
-
-export default requiresLogin()(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Dashboard)
-);
+export default requiresLogin()(Dashboard);
--- src/items/list/itemListView.js
+++ src/items/list/itemListView.js
@@ -1,21 +1,49 @@
 import React from 'react';
+import { getItems, selectItem } from '../item-actions';
+import { connect } from 'react-redux';
 
-export default function ItemListView(props) {
-  const listOfItems = props.itemList
-    ? props.itemList.map(item => (
-        <li onClick={item.selectedItem} key={item.id}>
-          {item.itemTitle}
-        </li>
-      ))
-    : null;
+export class ItemListView extends React.Component {
+  componentDidMount() {
+    this.props.getItems();
+    this.props.selectItem();
+  }
 
-  return (
-    <div className="list-nav">
-      <ul className="future-list">
-        <div className="future-hdr">How Long Until</div>
-        {listOfItems}
-        <div className="past-hdr">How Long Since</div>
-      </ul>
-    </div>
-  );
+  render() {
+    const setSelectedItem = item => {
+      return { selectedItem: item };
+    };
+
+    const listOfItems = this.props.itemList
+      ? this.props.itemList.map(item => (
+          <li onClick={() => setSelectedItem(item)} key={item.id}>
+            {item.itemTitle}
+          </li>
+        ))
+      : null;
+    return (
+      <div className="list-nav">
+        <ul className="future-list">
+          <div className="future-hdr">How Long Until</div>
+          {listOfItems}
+          <div className="past-hdr">How Long Since</div>
+        </ul>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    itemList: state.item.itemList,
+    selectedItem: state.item.selectedItem
+  };
+};
+const mapDispatchToProps = {
+  getItems,
+  selectItem
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ItemListView);
