e8ca34f7394c3a3b267e4cc85541bf35cd7bbc90
Bryan Spearman <bryanspearman@yahoo.com>
2019-01-27 15:12:26 -0500

    config fix

---
+++ src/auth/error-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class ErrorPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>An Error Occurred</h1>
+            </div>
+        );
+    }
+}
+
+export default ErrorPage;
