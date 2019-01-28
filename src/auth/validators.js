69471adf8ebc50e6305eb7f7bfc675a296b17107
Bryan Spearman <bryanspearman@yahoo.com>
2019-01-28 08:16:18 -0500

    sign up error css update

--- src/auth/validators.js
+++ src/auth/validators.js
@@ -1,17 +1,36 @@
-export const required = value => (value ? undefined : 'Required');
+export const required = value =>
+  value ? undefined : <span className="alert">Required</span>;
 export const nonEmpty = value =>
-    value.trim() !== '' ? undefined : 'Cannot be empty';
+  value.trim() !== '' ? (
+    undefined
+  ) : (
+    <span className="alert">Cannot be empty</span>
+  );
 export const isTrimmed = value =>
-    value.trim() === value ? undefined : 'Cannot start or end with whitespace';
+  value.trim() === value ? (
+    undefined
+  ) : (
+    <span className="alert">Cannot start or end with whitespace</span>
+  );
 export const length = length => value => {
-    if (length.min && value.length < length.min) {
-        return `Must be at least ${length.min} characters long`;
-    }
-    if (length.max && value.length > length.max) {
-        return `Must be at most ${length.max} characters long`;
-    }
+  if (length.min && value.length < length.min) {
+    return (
+      <span className="alert">
+        `Must be at least ${length.min} characters long`
+      </span>
+    );
+  }
+  if (length.max && value.length > length.max) {
+    return (
+      <span className="alert">
+        `Must be at most ${length.max} characters long`
+      </span>
+    );
+  }
 };
 export const matches = field => (value, allValues) =>
-    field in allValues && value.trim() === allValues[field].trim()
-        ? undefined
-        : 'Does not match';
+  field in allValues && value.trim() === allValues[field].trim() ? (
+    undefined
+  ) : (
+    <span className="alert">Does not match</span>
+  );
